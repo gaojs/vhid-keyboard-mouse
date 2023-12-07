d212534287e976f0d7b5888b8738569b311ec354
hill.gao <hill.gao@yuansuan.com>
2023-12-07 18:47:54 +0800

    测试成功（需要关闭SecureBoot，开启TestSigning）。 关闭SecureBoot：进入BIOS系统（Enter-F1），选择【Security-SecureBoot-off】，关闭安全启动，然后重启。 开启TestSigning：管理员身份执行命令（bcdedit -set TESTSIGNING ON），用命令（bcdedit -enum）查看，然后重启。

--- hitester/main.cpp
+++ hitester/main.cpp
@@ -8,35 +8,43 @@
 #include "mouse.h"
 #include "keyboard.h"
 
-int main()
-{
-    Mouse mouse;
-    try {
-        mouse.initialize();
-    } catch (const std::runtime_error &e) {
-        std::cout << std::string("Mouse initialization: ") + e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+int main() {
+  Mouse mouse;
+  try {
+    mouse.initialize();
+  } catch (const std::runtime_error &e) {
+    std::cout << std::string("Mouse init: ") + e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    Keyboard keyboard;
-    try {
-        keyboard.initialize();
-    } catch (const std::runtime_error &e) {
-        std::cout << std::string("Keyboard initialization: ") + e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-    std::cout << std::string("cursor move and click.") << std::endl;
-    Sleep(3000);
-    // point(x,y) is in screen(1920x768).
-    std::cout << "move to (1500,100) and click "<< std::endl;
-    mouse.moveCursor(1500, 100);
-    mouse.rightButtonClick();
-    Sleep(1000);
-    std::cout << "move to (1300,300) and click " << std::endl;
-    mouse.moveCursor(1300, 300);
-    mouse.rightButtonClick();
-    Sleep(1000);
+  Keyboard keyboard;
+  try {
+    keyboard.initialize();
+  } catch (const std::runtime_error &e) {
+    std::cout << std::string("Keyboard init: ") + e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout << std::string("cursor move and click. watch the cursor, pls!") << std::endl;
+  Sleep(1000);
+  // point(x,y) is in screen(1920x768).
+  std::cout << "move to (1500,100) and click " << std::endl;
+  Sleep(1000);
+  mouse.moveCursor(1500, 100);
+  mouse.rightButtonClick();
+  Sleep(1000);
+  std::cout << "move to (1400,200) and click " << std::endl;
+  Sleep(1000);
+  mouse.moveCursor(1400, 200);
+  mouse.rightButtonClick();
+  Sleep(1000);
+  std::cout << std::string("type KEY_CAPSLOCK 100 times. watch the caps_key_led, pls!") << std::endl;
+  keyboard.type(KEY_ESC);
+  Sleep(1000);
+  for (int i = 0; i < 100; i++) {
+    keyboard.type(KEY_CAPSLOCK);
+    Sleep(100);
+  }
 
-    // system("pause");
-    return EXIT_SUCCESS;
+  // system("pause");
+  return EXIT_SUCCESS;
 }
